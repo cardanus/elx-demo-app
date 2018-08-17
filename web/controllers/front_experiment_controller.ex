e14eb746a1366135a37e8e1f63508390ea305276
Oleksii D. Semilietov <spylik@gmail.com>
2018-08-17 14:58:40 +0300

    another one experiments

--- web/controllers/front_experiment_controller.ex
+++ web/controllers/front_experiment_controller.ex
@@ -3,18 +3,28 @@ defmodule Elxdemoapp.FrontExperimentController do
   alias Elxdemoapp.Helpers.UserId
 
   def index(conn, _params) do
-    button_content = conn
-      |> UserId.fetch()
-      |> :erlexp.variant("deposit_button_exp0") # "year_subscribtion" is experiment name
+     user_id = conn |> UserId.fetch()
+
+    button_content = user_id
+      |> :erlexp.variant("deposit_button_exp0") # "deposit_button_exp0" is experiment name
       |> gen_button_content()
+
+    button_class = user_id
+      |> :erlexp.variant("deposit_button_green") # "deposit_button_green" is experiment name
+      |> gen_button_class()
 
     render(
       conn, "index.html",
-      content: button_content
+      content: button_content,
+      button_class: button_class
     )
   end
 
-  def gen_button_content(:a), do: "Yes, I want!"
-  def gen_button_content(:b), do: "No! I want to earn 16%!"
+  def gen_button_content(:a), do: "Earn 18% with us!"
+  def gen_button_content(:b), do: "Save 18% with us!"
+
+  def gen_button_class(:a), do: ""
+  def gen_button_class(:b), do: "green"
+
 
 end
