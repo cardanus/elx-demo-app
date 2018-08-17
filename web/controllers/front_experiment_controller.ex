1eaa2fc7caabf2b3bcb127826f60e63758e05430
Oleksii D. Semilietov <spylik@gmail.com>
2018-08-17 14:03:20 +0300

    islandbanki demo app

--- web/controllers/front_experiment_controller.ex
+++ web/controllers/front_experiment_controller.ex
@@ -3,31 +3,18 @@ defmodule Elxdemoapp.FrontExperimentController do
   alias Elxdemoapp.Helpers.UserId
 
   def index(conn, _params) do
-    subscribe_info = conn
+    button_content = conn
       |> UserId.fetch()
-      |> :erlexp.variant("year_subscribtion") # "year_subscribtion" is experiment name
-      |> gen_price()
+      |> :erlexp.variant("deposit_button_exp0") # "year_subscribtion" is experiment name
+      |> gen_button_content()
 
     render(
       conn, "index.html",
-      price: subscribe_info.price,
-      period: subscribe_info.period,
-      period_desc: subscribe_info.period_desc
+      content: button_content
     )
   end
 
-  # old version with monthly price
-  def gen_price(:a) do
-    %{period: 1, period_desc: "month", price: 12 }
-  end
-
-  # new version with yearly price
-  def gen_price(:b) do
-    %{period: 1, period_desc: "year", price: 144 }
-  end
-
-  def show(conn, %{"id" => month}) do
-    render(conn, "show.html", price: month)
-  end
+  def gen_button_content(:a), do: "Yes, I want!"
+  def gen_button_content(:b), do: "No, but I want to earn 16%!"
 
 end
